d4dbc45791dc5a1d78def326cd599721ae24efa6
elzup <guild0105@gmail.com>
2019-12-18 14:33:31 +0900

    feat: loosen rules import

--- import.js
+++ import.js
@@ -66,7 +66,7 @@ module.exports = {
 		'import/first': 2,
 		// hmm... this rule appears to be an alias to import/first... But I gotta configure them allllllll!
 		'import/imports-first': 2,
-		'import/max-dependencies': [2, { max: 14 }],
+		'import/max-dependencies': 0,
 		'import/no-anonymous-default-export': 0,
 		'import/no-absolute-path': 2,
 		'import/no-deprecated': 1, // this is an in progress rule
@@ -75,7 +75,7 @@ module.exports = {
 		'import/no-internal-modules': 0,
 		'import/no-mutable-exports': 2,
 		'import/no-restricted-paths': 0,
-		'import/no-unassigned-import': 2,
+		'import/no-unassigned-import': 0,
 		'import/no-webpack-loader-syntax': 2,
 		// export defaultはrollupできず、追いづらくなる可能性がある
 		'import/prefer-default-export': 0,
