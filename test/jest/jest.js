396a5ed5571ab3d504d373b900e44297c5cb06fc
elzup <guild0105@gmail.com>
2020-11-09 20:27:33 +0900

    jest update

---
+++ test/jest/jest.js
@@ -0,0 +1,44 @@
+console.log('ok')
+
+// TODO
+
+// OK consistent-test-it
+// OK expect-expect
+// OK lowercase-name
+// OK no-alias-methods
+// OK no-commented-out-tests
+// OK no-conditional-expect
+// OK no-deprecated-functions
+// OK no-disabled-tests
+// OK no-done-callback
+// OK no-duplicate-hooks
+// OK no-export
+// OK no-focused-tests
+// OK no-hooks
+// OK no-identical-title
+// OK no-if
+// OK no-interpolation-in-snapshots
+// OK no-jasmine-globals
+// OK no-jest-import
+// OK no-large-snapshots
+// OK no-mocks-import
+// OK no-restricted-matchers
+// OK no-standalone-expect
+// OK no-test-prefixes
+// OK no-test-return-statement
+// OK prefer-called-with
+// OK prefer-expect-assertions
+// OK prefer-hooks-on-top
+// OK prefer-spy-on
+// OK prefer-strict-equal
+// OK prefer-to-be-null
+// OK prefer-to-be-undefined
+// OK prefer-to-contain
+// OK prefer-to-have-length
+// OK prefer-todo
+// OK require-to-throw-message
+// OK require-top-level-describe
+// OK valid-describe
+// OK valid-expect
+// OK valid-expect-in-promise
+// OK valid-title
